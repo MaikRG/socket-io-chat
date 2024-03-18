dbd4abb83b0a36cd34d42872aaa4e231721e5ad7
Miguel R.G <98700679+MaikRG@users.noreply.github.com>
2024-03-18 14:23:20 +0100

    feat: Add client and server scripts

---
+++ src/client.ts
@@ -0,0 +1,44 @@
+import { io } from 'socket.io-client'
+import * as readline from 'readline/promises';
+
+let nickname = 'unknown';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function main() {
+    const asnwer = await rl.question('What is your nickname: ');
+    nickname = asnwer.toString();
+    
+    const socket = io('ws://localhost:3001');
+
+    socket.on("welcome", (data) =>{
+        console.log(data.welcome);
+        data.messages.forEach( (data: {nickname: string, message: string, date: Date}) => {
+            console.log(`${data.nickname}: ${data.message}`)
+        });
+        rl.prompt();
+    })
+
+
+    socket.on("chat", (data) => {
+        console.log(`${data.nickname}: ${data.message}`)
+        rl.prompt();
+    })
+
+    rl.on('line', (input)=>{
+        const payload = {
+            nickname,
+            message: input
+        }
+        socket.emit('chat', payload);
+    })
+}
+
+
+
+
+
+main();
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import { Server } from 'socket.io'
+
+const PORT = 3001
+const io = new Server(PORT);
+console.log("Server listening on port: ", PORT);
+
+const messages: {nickname: string, message:string, date: Date}[] = [];
+
+io.on('connection', (socket) => {
+    console.log("New socket ID connection", socket.id);
+    const welcome = {
+        welcome: 'Welcome to global-chat!',
+        messages
+    }
+    socket.emit('welcome', welcome);
+
+    // Add socket in room
+    socket.join('global-chat');
+
+    // Listener to "chat" event
+    socket.on('chat', (data) => {
+        data.date = new Date();
+        console.log(data);
+        messages.push(data);
+        // Send 'data' to all joined sockets in room 'chat'
+        io.in('global-chat').emit('chat', data);
+    })
+})
+
+
+
+
+
+
+
+
